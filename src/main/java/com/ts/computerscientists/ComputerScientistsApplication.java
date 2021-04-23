e78f7e318e6bd3acf191d988a27d6ff2aaba7950
Tyler Rodriguez <tylerdeanusa@gmail.com>
2021-04-22 19:03:21 -0500

    first commit

---
+++ src/main/java/com/ts/computerscientists/ComputerScientistsApplication.java
@@ -0,0 +1,13 @@
+package com.ts.computerscientists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerScientistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerScientistsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ts/computerscientists/ComputerScientistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.computerscientists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerScientistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
